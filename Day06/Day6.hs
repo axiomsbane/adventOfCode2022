43fcc6bb5421b0c2fa6c4b543c03e3bc80630db0
Aditya Shidhaye <ashidhaye2@gmail.com>
2022-12-06 15:04:20 +0530

    Day 6 done

---
+++ Day06/Day6.hs
@@ -0,0 +1,17 @@
+#!/usr/bin/env stack
+{- stack script
+   --resolver lts-19.28
+   --package base,split,extra,containers,array
+-}
+
+import Data.Containers.ListUtils (nubOrd)
+globSz = 14
+
+sol :: [Char] -> Int -> Int
+sol lis ctr = case globSz == (length . nubOrd $ (take globSz lis)) of
+    True  -> (ctr + globSz - 1)
+    False -> sol (tail lis) (1 + ctr)
+
+main = do 
+    inp <- readFile "input.txt"
+    print $ sol inp 1
