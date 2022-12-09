dd7a5ebb9544cdd610f96db752f6d6ff30dc505e
Aditya Shidhaye <ashidhaye2@gmail.com>
2022-12-09 17:40:31 +0530

    Day 9 clean up

--- Day09/Day9.hs
+++ Day09/Day9.hs
@@ -9,24 +9,14 @@ import Data.Set hiding (map)
 import Prelude hiding (head,tail,sum)
 import Data.List (nub)
 
-
 type Vis = Set Pos
 newtype Pos = Pos {pos :: (Int, Int)}
-    deriving (Show)
+    deriving (Show,Eq,Ord)
 data Track = Track {head :: Pos, tail :: Pos}
     deriving (Show)
+    
 sToI :: String -> Int
 sToI = read
-
-instance Eq Pos where
-    (==) (Pos (x,y)) (Pos (z,w)) = x==z && y==w
-
-instance Ord Pos where 
-    compare :: Pos -> Pos -> Ordering
-    compare (Pos (x,y)) (Pos (z,w))
-        | x < y = LT
-        | x == y = EQ
-        | otherwise = GT
 
 instance Num Pos where 
     (+) (Pos (x,y)) (Pos (w,z)) = Pos (x+w,y+z)
@@ -52,16 +42,6 @@ calc trk@(Track he ta)
         (taX,taY) = pos ta
         hammDist = sum $ pos $ abs $ (he - ta)
 
-simulateHelp :: [Pos] -> Track -> [Track] -> [Track]
-simulateHelp [] _ vis = []
-simulateHelp (mov:movs) (Track he ta) mp = calTa : simulateHelp movs calTa mp
-    where 
-        calTa = calc (Track (he+mov) ta) 
-
-
---11111111111111111111111111111111111111111111111111111111111111111111111111111111111111
---11111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-
 --HeadKnot -> tailKnots -> updates posis of knots
 propRope :: Pos -> [Pos] -> [Pos]
 propRope _ [] = []
@@ -70,12 +50,16 @@ propRope he (x:xs) = newTe : propRope newTe xs
         tmp = calc $ Track he x 
         newTe = tail tmp
 
-simulateHelp2 :: [Pos] -> Pos -> [Pos] -> [Pos] -> [Pos]
-simulateHelp2 [] _ _ vis = []
-simulateHelp2 (mov:movs) curHe knots mp = (last updKnots) : simulateHelp2 movs updHe updKnots mp
+simulate :: [Pos] -> Pos -> [Pos] -> [Pos]
+simulate [] _ _ = []
+simulate (mov:movs) curHe knots = (last updKnots) : simulate movs updHe updKnots
     where 
         updHe = curHe + mov
         updKnots = propRope updHe knots
+
+zerPos = Pos (0,0)
+rope1 = [zerPos]
+rope2 = replicate 9 zerPos
 
 getPos :: String -> [Pos] 
 getPos ('L' : ' ' : x) = replicate (sToI x) (Pos (-1,0))
@@ -84,15 +68,12 @@ getPos ('U' : ' ' : x) = replicate (sToI x) (Pos (0,1))
 getPos ('D' : ' ' : x) = replicate (sToI x) (Pos (0,-1))
 getPos _ = []
 
-rope = replicate 9 zerPos
-zerPos = Pos (0,0)
-initialTrack = Track zerPos zerPos
 main = do 
     inp <- readFile "input.txt"
     let input = lines inp
         ins = concatMap getPos input 
-        solvHelp = simulateHelp ins initialTrack []
-        ans1 = size $ fromList $ map (pos . tail) solvHelp
-        solvHelp2 = simulateHelp2 ins zerPos rope []
+        solvHelp2 = simulate ins zerPos rope2
         ans2 = size $ fromList solvHelp2
-    print $ length $ nub solvHelp2
+        ans1 = size $ fromList $ simulate ins zerPos rope1
+    print ans1
+    print ans2
