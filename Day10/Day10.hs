1eaa6f00e5e6af41859595c2df4729875fe6f01c
Aditya Shidhaye <ashidhaye2@gmail.com>
2022-12-10 12:01:07 +0530

    Day 10 clean up

--- Day10/Day10.hs
+++ Day10/Day10.hs
@@ -14,29 +14,22 @@ parseIns str = case str of
     ('a':'d':'d':'x':' ':rem) -> [0, sToI rem]
     "noop"                    -> [0]
     _                         -> []
-
-idxs :: [[a] -> a]
-idxs = [(!! 19), (!! 59), (!! 99), (!! 139), (!! 179), (!! 219)]
 inds = [20,60,100,140,180,220]
-
-initPos = [1,2,3]
+indsMinus1 = map (+ (-1)) inds
 
 duringCycles = [0,1..239]
 
 solve :: [Int] -> Int -> Char
-solve op cyc = if (((cyc+1) `mod` 40) `elem` spriteSpan) then '#' else '.'
+solve registerVals cyc = if ((cyc+1) `mod` 40) `elem` spritePos then '#' else '.'
     where 
-        afterCycInit = op !! cyc 
-        spriteSpan = map ($ afterCycInit) [(+0),(+1),(+2)] 
-
+        spriteIdx1Pos = registerVals !! cyc 
+        spritePos = map ($ spriteIdx1Pos) [(+0),(+1),(+2)]
 main = do
     inp <- readFile "input.txt"
     let input = lines inp   
         ins = concatMap parseIns input
         outPut = scanl (+) 1 ins
-        ans1 = sum $ zipWith (*) inds $ map ($ outPut) idxs
-        charOp = map (solve outPut) duringCycles
-        chru = chunksOf 40 charOp
+        ans1 = sum $ zipWith (*) inds $ map (outPut !!) indsMinus1
+        crtOutput = chunksOf 40 $ map (solve outPut) duringCycles
     print ans1
-    mapM putStrLn chru
-
+    mapM putStrLn crtOutput
